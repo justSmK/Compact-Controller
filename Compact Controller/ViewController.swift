358a3a9156f0b99cd85d4f5cfeea991588b35268
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-14 11:48:48 +0300

    Project created

---
+++ Compact Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Compact Controller
+//
+//  Created by Sergei Semko on 5/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
